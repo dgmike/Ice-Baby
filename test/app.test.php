026f6242761333bc48f11c6f7351c0134e1a934a
Michael <michaelgranados@gmail.com>
2009-11-08 20:19:14 -0200

    Removendo quem tem mais de 80chars nos testes

--- test/app.test.php
+++ test/app.test.php
@@ -8,36 +8,45 @@ class AppTest extends UnitTestCase
     {
         ob_start(); app(array('.*' => 'BufferConstructTest'), '');
         $contents = get_content();
-        $this->assertEqual('Hello!', $contents, 'Foi chamada a classe BufferConstructTest. %s');
+        $this->assertEqual('Hello!', $contents,
+            'Foi chamada a classe BufferConstructTest. %s');
     }
 
     public function testCallGetPostAny()
     {
         ob_start(); app(array('.*' => 'BufferMethodTest'), '', 'GET');
         $contents = get_content();
-        $this->assertEqual('get', $contents, 'Foi chamado o metodo get pelo REQUEST. %s');
+        $this->assertEqual('get', $contents,
+            'Foi chamado o metodo get pelo REQUEST. %s');
 
         ob_start(); app(array('.*' => 'BufferMethodTest'), '', 'POST');
         $contents = get_content();
-        $this->assertEqual('post', $contents, 'Foi chamado o metodo post pelo REQUEST. %s');
+        $this->assertEqual('post', $contents,
+            'Foi chamado o metodo post pelo REQUEST. %s');
 
         ob_start(); app(array('.*' => 'BufferMethodTest'), '', 'ANY');
         $contents = get_content();
-        $this->assertEqual('any', $contents, 'Foi chamado o metodo any pelo REQUEST. %s');
+        $this->assertEqual('any', $contents,
+            'Foi chamado o metodo any pelo REQUEST. %s');
     }
 
     public function testError()
     {
         ob_start(); app(array('invalida' => 'BufferMethodTest'), '');
         $contents = get_content();
-        $this->assertEqual("Classe Erro\n404 - Page Not Found", $contents, 'A url nao foi encontrada na lista desejada. %s');
+        $this->assertEqual("Classe Erro\n404 - Page Not Found", $contents, 
+            'A url nao foi encontrada na lista desejada. %s');
     }
 
     public function testArgs()
     {
-        ob_start(); app(array('/(post|category)/(\d+)/?' => 'BufferMethodArgsTest'), '/post/25');
+        ob_start();
+        app(array(
+                '/(post|category)/(\d+)/?' => 'BufferMethodArgsTest'
+            ), '/post/25');
         $contents = get_content();
-        $this->assertEqual("Called get\n".var_export(array('post', '25'), true), $contents, 'Deveria retornar o metodo com os argumentos. %s');
+        $this->assertEqual("Called get\n".var_export(array('post', '25'), true),
+            $contents, 'Deveria retornar o metodo com os argumentos. %s');
     }
 
     public function testOnlyArray()
