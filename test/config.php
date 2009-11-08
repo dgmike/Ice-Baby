f860ec80d3fbb02d4b2a22549bf0d99277bf4fc2
Michael <michaelgranados@gmail.com>
2009-11-08 14:47:42 -0200

    Adicionando à lista de inclusao a pasta do projeto

--- test/config.php
+++ test/config.php
@@ -1,4 +1,9 @@
 <?php
+$path = dirname(dirname(__FILE__))
+        .DIRECTORY_SEPARATOR.'code';
+
+set_include_path($path.PATH_SEPARATOR.get_include_path());
+
 include_once dirname(dirname(__FILE__))
              .DIRECTORY_SEPARATOR.'simpletest'
              .DIRECTORY_SEPARATOR.'autorun.php';
