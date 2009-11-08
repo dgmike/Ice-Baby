11d23674dbcf26de071394098c25092aa2a169b7
Michael <michaelgranados@gmail.com>
2009-11-08 21:47:22 -0200

    Fazendo uma página de erro padrão

---
+++ code/ice/default_controller.php
@@ -0,0 +1,26 @@
+<?php
+if (!class_exists('Error')) {
+
+class Error
+{
+    public $error_code;
+    public $error_message;
+
+    function get()
+    {
+        print <<<EOF
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>{$this->error_code} {$this->error_message}</title>
+    </head>
+    <body>
+        <h1>{$this->error_code} - {$this->error_message}</h1>
+        <p>Please make contact with your administrator.</p>
+    </body>
+</html>
+EOF;
+    }
+}
+
+} // CLASS EXISTS ERROR
