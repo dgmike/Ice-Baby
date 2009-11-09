f4346a4a62c09473dfb28fd5553f7449ceee300f
Michael <michaelgranados@gmail.com>
2009-11-09 02:33:40 -0200

    SIM! Nos conseguimos fazer uma conexão apenas usando constantes ou passando argumentos. Nota: DB_DRIVER_OPTIOINS deve ser serializado já que o php não suporta arrays em suas constantes

--- code/ice/model.php
+++ code/ice/model.php
@@ -1,11 +1,12 @@
 <?php
-
 class Model extends PDO
 {
     public $_table = null;
     public $_key = null;
+    public $_conn;
 
-    public function __construct()
+    public function __construct($dns=null, $username=null, $password=null,
+            array $driver_options = array())
     {
         if (!$this->_table) {
             $this->_table = strtolower(get_class($this));
@@ -13,5 +14,19 @@ class Model extends PDO
         if (!$this->_key) {
             $this->_key = 'id_'.$this->_table;
         }
+        $items = array('dns', 'username', 'password', 'driver_options');
+        foreach ($items as $item) {
+            if (null==$$item) {
+                $item = strtoupper($item);
+                if (defined("DB_$item")) {
+                    if ($item == 'DRIVER_OPTIONS') {
+                        $driver_options = unserialize(DB_DRIVER_OPTIONS);
+                    } else {
+                        ${strtolower($item)} = constant("DB_$item");
+                    }
+                }
+            }
+        }
+        parent::__construct($dns, $username, $password, $driver_options);
     }
 }
--- test/database.php
+++ test/database.php
@@ -2,6 +2,10 @@
 include 'config.php';
 include 'ice/model.php';
 
+define('DB_DNS', 'sqlite://'
+                .dirname(__FILE__)
+                .DIRECTORY_SEPARATOR.'banco.db');
+
 class User extends Model {}
 
 class ModelTest extends UnitTestCase
@@ -15,6 +19,7 @@ class ModelTest extends UnitTestCase
     {
         $user = new User;
         $this->assertEqual('user', $user->_table, 'A tabela foi definida. %s');
-        $this->assertEqual('id_user', $user->_key, 'A chave primaria foi definida. %s');
+        $this->assertEqual('id_user', $user->_key, 
+                'A chave primaria foi definida. %s');
     }
 }
