ca2ded12cd79c2e16e2785259154634f8362ec78
Michael <mike@visie.com.br>
2010-02-11 18:19:58 -0200

    Adicionando o metodo noCache, muito util no dia-a-dia

--- code/ice/Ice.php
+++ code/ice/Ice.php
@@ -53,4 +53,21 @@ class Ice
         }
         return Ice::error(404, 'Page Not Found', $method);
     }
+
+    /**
+     * noCache - cleans the Expires Time to preserve the redirect cache
+     * 
+     * @static
+     * @access public
+     * @return void
+     */
+    static public function noCache()
+    {
+        header("Last-Modified: " . gmdate( "D, j M Y H:i:s" ) . " GMT"); // Date in the past 
+        header("Expires: " . gmdate( "D, j M Y H:i:s", time() ) . " GMT"); // always modified 
+        header("Cache-Control: no-store, no-cache, must-revalidate"); // HTTP/1.1 
+        header("Cache-Control: post-check=0, pre-check=0", FALSE); 
+        header("Pragma: no-cache");
+    }
+
 }
